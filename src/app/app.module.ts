0736f6d3e83c48bdbbcc10e19f3464346fdc22dc
Sifiso Lucolo Dhlamini <sifisodhlamini83@gmail.com>
2019-10-25 19:17:40 +0200

    From editor

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
+import { ProductService } from './product.service';
+import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductPageComponent,
+    ProductDescriptionComponent,
+    ProductTracklistingComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [ProductService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
